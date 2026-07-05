3bd90d3f196c492cfcdeb41b2e26c7f1c66d0719
abc2z <136438690+lummmmmn@users.noreply.github.com>
2026-07-05 08:25:26 +0700

    Refactor Footer component for improved structure

--- 92kamera/src/components/layout/Footer.jsx
+++ 92kamera/src/components/layout/Footer.jsx
@@ -132,7 +132,7 @@ export default function Footer({ isMobile, siteContent }) {
               textAlign: isMobile ? "center" : "right",
             }}
           >
-            © 2026 92 KA MÊ RA / abc2z
+            © 2026 92 KA MÊ RA / abc2z/v1.0
           </div>
         </div>
       </div>
